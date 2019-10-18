915b3109ea641803ef82a5b6e24572d4dc9114a3
tinkerer93 <a_golovko@i.ua>
2019-10-18 08:51:22 +0300

    Added unittests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
+from mock import patch
 from django.test import TestCase
+from posts.models import Post
+from posts.serializers import PostSerializer
+from users.serializers import UserSerializer
 
-# Create your tests here.
+
+class CreatePostViewTest(TestCase):
+    @patch('users.serializers.validate_email', return_value="mock@gmail.com")
+    @classmethod
+    def setUp(cls):
+        user = {
+            "first_name": "Jonh",
+            "last_name": "Doe",
+            "email": "mock@gmail.com",
+            "password": "12345"
+        }
+        serializer = UserSerializer
+        serializer.save(user)
+
